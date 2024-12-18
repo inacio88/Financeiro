6e71f7c3306ee384e4ef22431ad8856cc78d3e1c
inacio88 <inacio88@outlook.com>
2024-12-18 17:52:37 -0300

    main: config identity na program

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,6 +1,7 @@
 using Dima.Api.Data;
 using Dima.Api.EndPoints;
 using Dima.Api.Handlers;
+using Dima.Api.Models;
 using Dima.Core.Handlers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -25,13 +26,25 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(conexao);
 });
 
+builder.Services.AddIdentityCore<User>()
+                .AddRoles<IdentityRole<long>>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddApiEndpoints()
+                ;
 
 builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
 builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+
 app.UseSwagger();
 app.UseSwaggerUI();
+
+
 app.MapGet("/", () => new {message = "OK"});
 app.MapEndpoints();
 
