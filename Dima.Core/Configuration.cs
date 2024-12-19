d97a25698f7643b3fb917c1e80bd8569c5eb77b6
inacio88 <inacio88@outlook.com>
2024-12-19 09:56:32 -0300

    main: extension do builder

--- Dima.Core/Configuration.cs
+++ Dima.Core/Configuration.cs
@@ -5,6 +5,7 @@ namespace Dima.Core
         public const int DefaultPageSize = 25;
         public const int DefaultPageNumber = 1;
         public const int DefaultStatusCode = 200;
+        public static string ConnectionString {get;set;} = string.Empty;
 
     }
 }
