b4819ad71123f137f52df5784101f372e18320a5
inacio88 <inacio88@outlook.com>
2024-12-26 15:46:58 -0300

    main: cacading view e config backend url e add services na program

--- Dima.Web/Program.cs
+++ Dima.Web/Program.cs
@@ -3,14 +3,18 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Dima.Web;
 using MudBlazor.Services;
 using Dima.Web.Security;
+using Microsoft.AspNetCore.Components.Authorization;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+Configuration.BackendUrl = builder.Configuration.GetValue<string>("Backendurl") ?? throw new Exception("Não foi encontrada backendurl");
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<CookieHandler>();
-
+builder.Services.AddScoped<AuthenticationStateProvider, CookieAuthenticationStateProvider>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped( x => (ICookieAuthenticationStateProvider) x.GetRequiredService<AuthenticationState>());
 builder.Services.AddMudServices();
 
 builder.Services.AddHttpClient(Configuration.HttpClientName, opt => 
