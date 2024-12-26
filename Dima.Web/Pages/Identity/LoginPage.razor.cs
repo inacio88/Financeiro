9cfa51df3218679f0a8922d25b305e59efe110cf
inacio88 <inacio88@outlook.com>
2024-12-26 18:39:27 -0300

    main: logout

---
+++ Dima.Web/Pages/Identity/LoginPage.razor.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Components;
+using Dima.Core.Handlers;
+using Dima.Core.Requests.Account;
+using MudBlazor;
+using Dima.Web.Security;
+namespace Dima.Web.Pages.Identity
+{
+    public partial class LoginPage : ComponentBase
+    {
+        [Inject]
+        public ICookieAuthenticationStateProvider authStateProvider { get; set; } = null!;
+        [Inject]
+        public IAccountHandler handler { get; set; } = null!;
+        [Inject]
+        public ISnackbar snackbar { get; set; } = null!;
+        [Inject]
+        public NavigationManager navigationManager { get; set; } = null!;
+
+
+        public LoginRequest InputModel { get; set; } = new();
+        public bool IsBusy { get; set; } = false;
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await authStateProvider.GetAuthenticationStateAsync();
+            var user = authState.User;
+
+            if (user.Identity is { IsAuthenticated: true })
+                navigationManager.NavigateTo("/");
+        }
+
+        public async Task OnValidSubmitAsync()
+        {
+            IsBusy = true;
+
+            try
+            {
+                var result = await handler.LoginAsync(InputModel);
+                if (result.IsSuccess)
+                {
+                    snackbar.Add(result.Message, Severity.Success);
+                    navigationManager.NavigateTo("/");
+                }
+                else
+                    snackbar.Add(result.Message, Severity.Error);
+            }
+            catch (Exception ex)
+            {
+                snackbar.Add(ex.Message, Severity.Error);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
---
+++ Dima.Web/Pages/Identity/LogoutPage.razor.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Components;
+using Dima.Core.Handlers;
+using MudBlazor;
+using Dima.Web.Security;
+namespace Dima.Web.Pages.Identity
+{
+    public partial class LogoutPage : ComponentBase
+    {
+        [Inject]
+        public ICookieAuthenticationStateProvider authStateProvider { get; set; } = null!;
+        [Inject]
+        public IAccountHandler handler { get; set; } = null!;
+        [Inject]
+        public ISnackbar snackbar { get; set; } = null!;
+        [Inject]
+        public NavigationManager navigationManager { get; set; } = null!;
+        
+        protected override async Task OnInitializedAsync()
+        {
+            if (await authStateProvider.CheckAuthenticatedAsync())
+            {
+                await handler.LogoutAsync();
+                await authStateProvider.GetAuthenticationStateAsync();
+                authStateProvider.NotifyAuthenticationStateChanged();
+            }
+
+            await base.OnInitializedAsync();
+        }
+    }
+}
